dbb50953fe062320583e2f3347ba7ee66e572fb3
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-04-18 21:47:36 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = "C:/UpWork Projects/engineer-cloud-nprod-cloud-run.json"
   project     = "engineer-cloud-nprod"
   region      = "us-central1"
   zone        = "us-central1-c"
